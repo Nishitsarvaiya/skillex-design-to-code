ead1be383f818869f21ccb19459505c48c8a1d48
nishitsarvaiya <nishitsarvaiiya@gmail.com>
2021-12-10 21:23:36 +0530

    layout done

--- src/App.js
+++ src/App.js
@@ -7,184 +7,219 @@ import Skills from './assets/images/skills.jpg';
 import { useEffect, useState } from 'react';
 
 const App = () => {
-    const [activePassion, setActivePassion] = useState(1);
+	const [activePassion, setActivePassion] = useState(1);
 
-    useEffect(() => {
-        const onMouseEnter = (e) => {
-            setActivePassion(e.target.dataset.item);
-        };
+	useEffect(() => {
+		const onMouseEnter = (e) => {
+			setActivePassion(e.target.dataset.item);
+		};
 
-        const onMouseLeave = (e) => {
-            setActivePassion(1);
-        };
+		const onMouseLeave = (e) => {
+			setActivePassion(1);
+		};
 
-        document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseenter', onMouseEnter));
-        document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseleave', onMouseLeave));
+		document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseenter', onMouseEnter));
+		document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseleave', onMouseLeave));
 
-        return () => {
-            document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseenter'));
-            document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseleave'));
-        };
-    }, []);
+		return () => {
+			document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseenter'));
+			document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseleave'));
+		};
+	}, []);
 
-    return (
-        <div className='App'>
-            <Header />
-            <main>
-                <section className='hero-section'>
-                    <div className='container'>
-                        <div className='hero'>
-                            <div className='hero-title'>
-                                <h1>
-                                    Watch.
-                                    <br />
-                                    Learn.
-                                    <br />
-                                    Grow.
-                                </h1>
-                            </div>
-                            <div className='custom-input'>
-                                <input type='text' placeholder='Find you passion' />
-                                <a href='#'>
-                                    <span>Go</span>
-                                </a>
-                            </div>
-                            <div className='hero-grid'>
-                                <div
-                                    className={activePassion === 1 ? 'grid-item grid-item-active' : 'grid-item'}
-                                    data-item='1'>
-                                    <div className='grid-item-gradient'></div>
-                                    <div className='grid-item-image'>
-                                        <img src={Person1} alt='' />
-                                    </div>
-                                    <div className='grid-item-info-active'>
-                                        <h1>
-                                            Writing
-                                            <br />
-                                            Course
-                                        </h1>
-                                        <h1>
-                                            100<p>Topics</p>
-                                        </h1>
-                                    </div>
-                                </div>
-                                <div
-                                    className={activePassion === 2 ? 'grid-item grid-item-active' : 'grid-item'}
-                                    data-item='2'>
-                                    <div className='grid-item-gradient'></div>
-                                    <div className='grid-item-image'>
-                                        <img src={Person2} alt='' />
-                                    </div>
-                                    <div className='grid-item-info'>
-                                        <div className='box'></div>
-                                        <h1>Writing</h1>
-                                    </div>
-                                    <div className='grid-item-info-active'>
-                                        <h1>
-                                            Writing
-                                            <br />
-                                            Course
-                                        </h1>
-                                        <h1>
-                                            100<p>Topics</p>
-                                        </h1>
-                                    </div>
-                                </div>
-                                <div
-                                    className={activePassion === 3 ? 'grid-item grid-item-active' : 'grid-item'}
-                                    data-item='3'>
-                                    <div className='grid-item-gradient'></div>
-                                    <div className='grid-item-image'>
-                                        <img src={Person3} alt='' />
-                                    </div>
-                                    <div className='grid-item-info'>
-                                        <div className='box'></div>
-                                        <h1>Business</h1>
-                                    </div>
-                                    <div className='grid-item-info-active'>
-                                        <h1>
-                                            Business
-                                            <br />
-                                            Course
-                                        </h1>
-                                        <h1>
-                                            100<p>Topics</p>
-                                        </h1>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section className='categories-section'>
-                    <div className='section-head'>
-                        <h1>
-                            Unlimited access to 100+ instructors<span className='green-dot'>.</span>
-                        </h1>
-                    </div>
-                    <Tabs />
-                </section>
-                <section className="skills-section">
-                    <div className="container">
-                        <div className="skills">
-                            <h1 className='skills-title'>
-                                Get the skills you
-                                <br />
-                                need for a job that
-                                <br />
-                                is in demand<span className='green-dot'>.</span>
-                            </h1>
-                            <div className="skills-content">
-                                <div className="skills-list">
-                                    <div className="skill">
-                                        <div className="skill-icon">
-                                            <img src="https://img.icons8.com/windows/24/faa789/prize.png" alt='leadership'/>
-                                        </div>
-                                        <div className="skill-info">
-                                            <h3>Leadership</h3>
-                                            <p>Fully committed to the success of the company</p>
-                                        </div>
-                                    </div>
-                                    <div className="skill">
-                                        <div className="skill-icon">
-                                            <img src="https://img.icons8.com/windows/24/faa789/brain.png" alt='Responsibility'/>
-                                        </div>
-                                        <div className="skill-info">
-                                            <h3>Responsibility</h3>
-                                            <p>Employees will always be my top priority</p>
-                                        </div>
-                                    </div>
-                                    <div className="skill">
-                                        <div className="skill-icon">
-                                            <img src="https://img.icons8.com/material-outlined/24/faa789/lightning-bolt.png" alt='Flexibility'/>
-                                        </div>
-                                        <div className="skill-info">
-                                            <h3>Flexibility</h3>
-                                            <p>The ability to switch is an important skill</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="skills-video">
-                                    <div className="video">
-                                        <img src={Skills} alt="skills" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section className='customer-section'>
-                    <div className='section-head'>
-                        <h1>
-                            What our customers say<span className='green-dot'>.</span>
-                        </h1>
-                    </div>
-                    <Carousel />
-                </section>
-            </main>
-            <Footer />
-        </div>
-    );
+	return (
+		<div className='App'>
+			<Header />
+			<main>
+				<section className='hero-section'>
+					<div className='container'>
+						<div className='hero'>
+							<div className='hero-title'>
+								<h1>
+									Watch.
+									<br />
+									Learn.
+									<br />
+									Grow.
+								</h1>
+							</div>
+							<div className='custom-input'>
+								<input type='text' placeholder='Find you passion' />
+								<a href='#'>
+									<span>Go</span>
+								</a>
+							</div>
+							<div className='hero-grid'>
+								<div
+									className={activePassion === 1 ? 'grid-item grid-item-active' : 'grid-item'}
+									data-item='1'>
+									<div className='grid-item-gradient'></div>
+									<div className='grid-item-image'>
+										<img src={Person1} alt='' />
+									</div>
+									<div className='grid-item-info-active'>
+										<h1>
+											Writing
+											<br />
+											Course
+										</h1>
+										<h1>
+											100<p>Topics</p>
+										</h1>
+									</div>
+								</div>
+								<div
+									className={activePassion === 2 ? 'grid-item grid-item-active' : 'grid-item'}
+									data-item='2'>
+									<div className='grid-item-gradient'></div>
+									<div className='grid-item-image'>
+										<img src={Person2} alt='' />
+									</div>
+									<div className='grid-item-info'>
+										<div className='box'></div>
+										<h1>Writing</h1>
+									</div>
+									<div className='grid-item-info-active'>
+										<h1>
+											Writing
+											<br />
+											Course
+										</h1>
+										<h1>
+											100<p>Topics</p>
+										</h1>
+									</div>
+								</div>
+								<div
+									className={activePassion === 3 ? 'grid-item grid-item-active' : 'grid-item'}
+									data-item='3'>
+									<div className='grid-item-gradient'></div>
+									<div className='grid-item-image'>
+										<img src={Person3} alt='' />
+									</div>
+									<div className='grid-item-info'>
+										<div className='box'></div>
+										<h1>Business</h1>
+									</div>
+									<div className='grid-item-info-active'>
+										<h1>
+											Business
+											<br />
+											Course
+										</h1>
+										<h1>
+											100<p>Topics</p>
+										</h1>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section className='categories-section'>
+					<div className='section-head'>
+						<h1>
+							Unlimited access to 100+ instructors<span className='green-dot'>.</span>
+						</h1>
+					</div>
+					<Tabs />
+				</section>
+				<section className='skills-section'>
+					<div className='container'>
+						<div className='skills'>
+							<h1 className='skills-title'>
+								Get the skills you
+								<br />
+								need for a job that
+								<br />
+								is in demand<span className='green-dot'>.</span>
+							</h1>
+							<div className='skills-content'>
+								<div className='skills-list'>
+									<div className='skill'>
+										<div className='skill-icon'>
+											<img
+												src='https://img.icons8.com/windows/24/faa789/prize.png'
+												alt='leadership'
+											/>
+										</div>
+										<div className='skill-info'>
+											<h3>Leadership</h3>
+											<p>Fully committed to the success of the company</p>
+										</div>
+									</div>
+									<div className='skill'>
+										<div className='skill-icon'>
+											<img
+												src='https://img.icons8.com/windows/24/faa789/brain.png'
+												alt='Responsibility'
+											/>
+										</div>
+										<div className='skill-info'>
+											<h3>Responsibility</h3>
+											<p>Employees will always be my top priority</p>
+										</div>
+									</div>
+									<div className='skill'>
+										<div className='skill-icon'>
+											<img
+												src='https://img.icons8.com/material-outlined/24/faa789/lightning-bolt.png'
+												alt='Flexibility'
+											/>
+										</div>
+										<div className='skill-info'>
+											<h3>Flexibility</h3>
+											<p>The ability to switch is an important skill</p>
+										</div>
+									</div>
+								</div>
+								<div className='skills-video'>
+									<div className='video'>
+										<img src={Skills} alt='skills' />
+									</div>
+									<div className='numbers'>
+										<div className='numbers-text'>
+											<p>
+												The modern labor market dictates its own terms. <br />
+												Today, to be a competitive specialist requires more than professional
+												skills.
+											</p>
+										</div>
+										<div className='numbers-details'>
+											<div className='numbers-details-item'>
+												<h1>10</h1>
+												<div>
+													<span>years</span>
+													<span>experience</span>
+												</div>
+											</div>
+											<div className='numbers-details-item line'></div>
+											<div className='numbers-details-item'>
+												<h1>250</h1>
+												<div>
+													<span>types of</span>
+													<span>courses</span>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section className='customer-section'>
+					<div className='section-head'>
+						<h1>
+							What our customers say<span className='green-dot'>.</span>
+						</h1>
+					</div>
+					<Carousel />
+				</section>
+			</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -2,142 +2,142 @@ import { useEffect, useState } from 'react';
 import { Card } from '..';
 
 const Carousel = () => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPage] = useState(0);
-    const [people, setPeople] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [totalPages, setTotalPage] = useState(0);
+	const [people, setPeople] = useState([]);
 
-    const goLeft = () => (currentPage === 1 ? console.log('end') : setCurrentPage(currentPage - 1));
+	const goLeft = () => (currentPage === 1 ? console.log('end') : setCurrentPage(currentPage - 1));
 
-    const goRight = () => (currentPage === totalPages - 1 ? console.log('end') : setCurrentPage(currentPage + 1));
+	const goRight = () => (currentPage === totalPages - 1 ? console.log('end') : setCurrentPage(currentPage + 1));
 
-    useEffect(() => {
-        setPeople([
-            {
-                id: 1,
-                name: 'Rob Zuber',
-                position: 'CEO',
-                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-            },
-            {
-                id: 2,
-                name: 'Rob Zuber',
-                position: 'CEO',
-                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-            },
-            {
-                id: 3,
-                name: 'Rob Zuber',
-                position: 'CEO',
-                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-            },
-            {
-                id: 4,
-                name: 'Rob Zuber',
-                position: 'CEO',
-                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-            },
-            {
-                id: 5,
-                name: 'Rob Zuber',
-                position: 'CEO',
-                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-            },
-            {
-                id: 6,
-                name: 'Rob Zuber',
-                position: 'CEO',
-                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-            },
-            {
-                id: 7,
-                name: 'Rob Zuber',
-                position: 'CEO',
-                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-            },
-            {
-                id: 8,
-                name: 'Rob Zuber',
-                position: 'CEO',
-                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-            },
-            {
-                id: 9,
-                name: 'Rob Zuber',
-                position: 'CEO',
-                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-            },
-            {
-                id: 10,
-                name: 'Rob Zuber',
-                position: 'CEO',
-                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-            },
-        ]);
-    }, []);
+	useEffect(() => {
+		setPeople([
+			{
+				id: 1,
+				name: 'Rob Zuber',
+				position: 'CEO',
+				text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
+			},
+			{
+				id: 2,
+				name: 'Melanie Pickett',
+				position: 'IT Manager',
+				text: 'I learned a lot of new things, I immediately apply my knowledge in my work, found myself a new client',
+			},
+			{
+				id: 3,
+				name: 'Regis Wilson',
+				position: 'Engineer',
+				text: 'I really like the speakers of the course and the quality of the lectures. There is always feedback',
+			},
+			{
+				id: 4,
+				name: 'Emma Watson',
+				position: 'Cook',
+				text: 'A new profession is a lot of vivid impressions. Today I was at the presentation of the regional business award',
+			},
+			{
+				id: 5,
+				name: 'Emma Watson',
+				position: 'CEO',
+				text: 'The course shows all the stages of the work of an SMM marketer. And also excellent practice',
+			},
+			{
+				id: 6,
+				name: 'Chris Hemsworth',
+				position: 'CEO',
+				text: 'On the course "Data Analyst in Python" everything is very interesting and informative, very detailed material',
+			},
+			{
+				id: 7,
+				name: 'Andrew Garfield',
+				position: 'CEO',
+				text: 'For several months I studied one of the most popular programming languages on the course "Java developer"',
+			},
+			{
+				id: 8,
+				name: 'Elizabeth Stone',
+				position: 'CEO',
+				text: 'The course shows all the stages of the work of an SMM marketer. And also excellent practice',
+			},
+			{
+				id: 9,
+				name: 'Chris Hemsworth',
+				position: 'CEO',
+				text: 'On the course "Data Analyst in Python" everything is very interesting and informative, very detailed material',
+			},
+			{
+				id: 10,
+				name: 'John Cena',
+				position: 'CEO',
+				text: 'For several months I studied one of the most popular programming languages on the course "Java developer"',
+			},
+		]);
+	}, []);
 
-    useEffect(() => {
-        setTotalPage(() => Math.ceil(people.length / 2));
-    }, [people]);
+	useEffect(() => {
+		setTotalPage(() => Math.ceil(people.length / 2));
+	}, [people]);
 
-    useEffect(() => {
-        const shiftCarousel = (currentPage) => {
-            let carousel = document.querySelector('.carousel');
-            if (carousel.children[0]) {
-                let carouselItemWidth = carousel.children[0].getBoundingClientRect().width;
-                carousel.style.transform = `translate3d(calc(${-(carouselItemWidth * 2) * currentPage}px - ${
-                    4 * currentPage
-                }rem), 0, 0`;
-            }
-        };
+	useEffect(() => {
+		const shiftCarousel = (currentPage) => {
+			let carousel = document.querySelector('.carousel');
+			if (carousel.children[0]) {
+				let carouselItemWidth = carousel.children[0].getBoundingClientRect().width;
+				carousel.style.transform = `translate3d(calc(${-(carouselItemWidth * 2) * currentPage}px - ${
+					4 * currentPage
+				}rem), 0, 0`;
+			}
+		};
 
-        shiftCarousel(currentPage - 1);
-    }, [currentPage]);
+		shiftCarousel(currentPage - 1);
+	}, [currentPage]);
 
-    return (
-        <div className='carousel-container'>
-            <div className='container'>
-                <div className='carousel'>
-                    {people.map((person) => (
-                        <div key={person.id} className='carousel-item'>
-                            <Card person={person} />
-                        </div>
-                    ))}
-                </div>
-                <div className='carousel-nav'>
-                    <button onClick={goLeft}>
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            className='h-6 w-6'
-                            fill='none'
-                            viewBox='0 0 24 24'
-                            stroke='currentColor'>
-                            <path
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                                strokeWidth='2'
-                                d='M10 19l-7-7m0 0l7-7m-7 7h18'
-                            />
-                        </svg>
-                    </button>
-                    <button onClick={goRight}>
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            className='h-6 w-6'
-                            fill='none'
-                            viewBox='0 0 24 24'
-                            stroke='currentColor'>
-                            <path
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                                strokeWidth='2'
-                                d='M14 5l7 7m0 0l-7 7m7-7H3'
-                            />
-                        </svg>
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className='carousel-container'>
+			<div className='container'>
+				<div className='carousel'>
+					{people.map((person) => (
+						<div key={person.id} className='carousel-item'>
+							<Card person={person} />
+						</div>
+					))}
+				</div>
+				<div className='carousel-nav'>
+					<button onClick={goLeft}>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							className='h-6 w-6'
+							fill='none'
+							viewBox='0 0 24 24'
+							stroke='currentColor'>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth='2'
+								d='M10 19l-7-7m0 0l7-7m-7 7h18'
+							/>
+						</svg>
+					</button>
+					<button onClick={goRight}>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							className='h-6 w-6'
+							fill='none'
+							viewBox='0 0 24 24'
+							stroke='currentColor'>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth='2'
+								d='M14 5l7 7m0 0l-7 7m7-7H3'
+							/>
+						</svg>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Carousel;
