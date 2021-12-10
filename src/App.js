272d5c7171daae506dae7932be479bd3f2bc0865
nishit <nishit@avdevs.com>
2021-12-10 14:26:02 +0530

    hero section

--- src/App.js
+++ src/App.js
@@ -3,113 +3,138 @@ import './assets/styles/app.scss';
 import Person1 from './assets/images/person-1.jpg';
 import Person2 from './assets/images/person-2.jpg';
 import Person3 from './assets/images/person-3.jpg';
+import { useEffect, useState } from 'react';
 
 const App = () => {
-	return (
-		<div className='App'>
-			<Header />
-			<main>
-				<section className='hero-section'>
-					<div className='container'>
-						<div className='hero'>
-							<div className='hero-left'>
-								<div className='hero-title'>
-									<h1>
-										Watch.
-										<br />
-										Learn.
-										<br />
-										Grow.
-									</h1>
-								</div>
-								<form>
-									<div className='custom-input'>
-										<input type='text' placeholder='Find you passion' />
-										<a href='#'>Go</a>
-									</div>
-								</form>
-							</div>
-							<div className='hero-right'>
-								<div className='hero-grid'>
-									<div className='grid-item grid-item-active'>
-										<div className='grid-item-image'>
-											<img src={Person1} alt='' />
-										</div>
-										<div className='grid-item-info'>
-											<h1>Writing</h1>
-										</div>
-										<div className='grid-item-info-active'>
-											<h1>
-												Writing
-												<br />
-												Course
-											</h1>
-											<h1>
-												100<p>Topics</p>
-											</h1>
-										</div>
-									</div>
-									<div className='grid-item'>
-										<div className='grid-item-image'>
-											<img src={Person2} alt='' />
-										</div>
-										<div className='grid-item-info'>
-											<h1>Writing</h1>
-										</div>
-										<div className='grid-item-info-active'>
-											<h1>
-												Writing
-												<br />
-												Course
-											</h1>
-											<h1>
-												100<p>Topics</p>
-											</h1>
-										</div>
-									</div>
-									<div className='grid-item'>
-										<div className='grid-item-image'>
-											<img src={Person3} alt='' />
-										</div>
-										<div className='grid-item-info'>
-											<h1>Business</h1>
-										</div>
-										<div className='grid-item-info-active'>
-											<h1>
-												Business
-												<br />
-												Course
-											</h1>
-											<h1>
-												100<p>Topics</p>
-											</h1>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-				<section className='categories-section'>
-					<div className='section-head'>
-						<h1>
-							Unlimited access to 100+ instructors<span className='green-dot'>.</span>
-						</h1>
-					</div>
-					<Tabs />
-				</section>
-				<section className='customer-section'>
-					<div className='section-head'>
-						<h1>
-							What our customers say<span className='green-dot'>.</span>
-						</h1>
-					</div>
-					<Carousel />
-				</section>
-			</main>
-			<Footer />
-		</div>
-	);
+    const [activePassion, setActivePassion] = useState(1);
+
+    useEffect(() => {
+        const onMouseEnter = (e) => {
+            setActivePassion(e.target.dataset.item);
+        };
+
+        const onMouseLeave = (e) => {
+            setActivePassion(1);
+        };
+
+        document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseenter', onMouseEnter));
+        document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseleave', onMouseLeave));
+
+        return () => {
+            document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseenter'));
+            document.querySelectorAll('.grid-item').forEach((item) => item.addEventListener('mouseleave'));
+        };
+    }, []);
+
+    return (
+        <div className='App'>
+            <Header />
+            <main>
+                <section className='hero-section'>
+                    <div className='container'>
+                        <div className='hero'>
+                            <div className='hero-title'>
+                                <h1>
+                                    Watch.
+                                    <br />
+                                    Learn.
+                                    <br />
+                                    Grow.
+                                </h1>
+                            </div>
+                            <form>
+                                <div className='custom-input'>
+                                    <input type='text' placeholder='Find you passion' />
+                                    <a href='#'>Go</a>
+                                </div>
+                            </form>
+                            <div className='hero-grid'>
+                                <div
+                                    className={activePassion === 1 ? 'grid-item grid-item-active' : 'grid-item'}
+                                    data-item='1'>
+                                    <div className='grid-item-gradient'></div>
+                                    <div className='grid-item-image'>
+                                        <img src={Person1} alt='' />
+                                    </div>
+                                    <div className='grid-item-info-active'>
+                                        <h1>
+                                            Writing
+                                            <br />
+                                            Course
+                                        </h1>
+                                        <h1>
+                                            100<p>Topics</p>
+                                        </h1>
+                                    </div>
+                                </div>
+                                <div
+                                    className={activePassion === 2 ? 'grid-item grid-item-active' : 'grid-item'}
+                                    data-item='2'>
+                                    <div className='grid-item-gradient'></div>
+                                    <div className='grid-item-image'>
+                                        <img src={Person2} alt='' />
+                                    </div>
+                                    <div className='grid-item-info'>
+                                        <div className='box'></div>
+                                        <h1>Writing</h1>
+                                    </div>
+                                    <div className='grid-item-info-active'>
+                                        <h1>
+                                            Writing
+                                            <br />
+                                            Course
+                                        </h1>
+                                        <h1>
+                                            100<p>Topics</p>
+                                        </h1>
+                                    </div>
+                                </div>
+                                <div
+                                    className={activePassion === 3 ? 'grid-item grid-item-active' : 'grid-item'}
+                                    data-item='3'>
+                                    <div className='grid-item-gradient'></div>
+                                    <div className='grid-item-image'>
+                                        <img src={Person3} alt='' />
+                                    </div>
+                                    <div className='grid-item-info'>
+                                        <div className='box'></div>
+                                        <h1>Business</h1>
+                                    </div>
+                                    <div className='grid-item-info-active'>
+                                        <h1>
+                                            Business
+                                            <br />
+                                            Course
+                                        </h1>
+                                        <h1>
+                                            100<p>Topics</p>
+                                        </h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className='categories-section'>
+                    <div className='section-head'>
+                        <h1>
+                            Unlimited access to 100+ instructors<span className='green-dot'>.</span>
+                        </h1>
+                    </div>
+                    <Tabs />
+                </section>
+                <section className='customer-section'>
+                    <div className='section-head'>
+                        <h1>
+                            What our customers say<span className='green-dot'>.</span>
+                        </h1>
+                    </div>
+                    <Carousel />
+                </section>
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -2,140 +2,142 @@ import { useEffect, useState } from 'react';
 import { Card } from '..';
 
 const Carousel = () => {
-	const [currentPage, setCurrentPage] = useState(1);
-	const [totalPages, setTotalPage] = useState(0);
-	const [people, setPeople] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPage] = useState(0);
+    const [people, setPeople] = useState([]);
 
-	const goLeft = () => (currentPage === 1 ? console.log('end') : setCurrentPage(currentPage - 1));
+    const goLeft = () => (currentPage === 1 ? console.log('end') : setCurrentPage(currentPage - 1));
 
-	const goRight = () => (currentPage === totalPages - 1 ? console.log('end') : setCurrentPage(currentPage + 1));
+    const goRight = () => (currentPage === totalPages - 1 ? console.log('end') : setCurrentPage(currentPage + 1));
 
-	const shiftCarousel = (currentPage) => {
-		let carousel = document.querySelector('.carousel');
-		let carouselItemWidth = document.querySelector('.carousel-item').getBoundingClientRect().width;
-		carousel.style.transform = `translate3d(calc(${-(carouselItemWidth * 2) * currentPage}px - ${
-			4 * currentPage
-		}rem), 0, 0`;
-	};
+    useEffect(() => {
+        setPeople([
+            {
+                id: 1,
+                name: 'Rob Zuber',
+                position: 'CEO',
+                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
+            },
+            {
+                id: 2,
+                name: 'Rob Zuber',
+                position: 'CEO',
+                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
+            },
+            {
+                id: 3,
+                name: 'Rob Zuber',
+                position: 'CEO',
+                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
+            },
+            {
+                id: 4,
+                name: 'Rob Zuber',
+                position: 'CEO',
+                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
+            },
+            {
+                id: 5,
+                name: 'Rob Zuber',
+                position: 'CEO',
+                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
+            },
+            {
+                id: 6,
+                name: 'Rob Zuber',
+                position: 'CEO',
+                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
+            },
+            {
+                id: 7,
+                name: 'Rob Zuber',
+                position: 'CEO',
+                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
+            },
+            {
+                id: 8,
+                name: 'Rob Zuber',
+                position: 'CEO',
+                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
+            },
+            {
+                id: 9,
+                name: 'Rob Zuber',
+                position: 'CEO',
+                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
+            },
+            {
+                id: 10,
+                name: 'Rob Zuber',
+                position: 'CEO',
+                text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
+            },
+        ]);
+    }, []);
 
-	useEffect(() => {
-		setPeople([
-			{
-				id: 1,
-				name: 'Rob Zuber',
-				position: 'CEO',
-				text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-			},
-			{
-				id: 2,
-				name: 'Rob Zuber',
-				position: 'CEO',
-				text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-			},
-			{
-				id: 3,
-				name: 'Rob Zuber',
-				position: 'CEO',
-				text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-			},
-			{
-				id: 4,
-				name: 'Rob Zuber',
-				position: 'CEO',
-				text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-			},
-			{
-				id: 5,
-				name: 'Rob Zuber',
-				position: 'CEO',
-				text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-			},
-			{
-				id: 6,
-				name: 'Rob Zuber',
-				position: 'CEO',
-				text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-			},
-			{
-				id: 7,
-				name: 'Rob Zuber',
-				position: 'CEO',
-				text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-			},
-			{
-				id: 8,
-				name: 'Rob Zuber',
-				position: 'CEO',
-				text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-			},
-			{
-				id: 9,
-				name: 'Rob Zuber',
-				position: 'CEO',
-				text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-			},
-			{
-				id: 10,
-				name: 'Rob Zuber',
-				position: 'CEO',
-				text: 'The explanation are clear, the teachers are responsible and friendly, and the homework is real practice',
-			},
-		]);
-	}, []);
+    useEffect(() => {
+        setTotalPage(() => Math.ceil(people.length / 2));
+    }, [people]);
 
-	useEffect(() => {
-		setTotalPage(() => Math.ceil(people.length / 2));
-	}, [people]);
+    useEffect(() => {
+        const shiftCarousel = (currentPage) => {
+            let carousel = document.querySelector('.carousel');
+            if (carousel.children[0]) {
+                let carouselItemWidth = carousel.children[0].getBoundingClientRect().width;
+                carousel.style.transform = `translate3d(calc(${-(carouselItemWidth * 2) * currentPage}px - ${
+                    4 * currentPage
+                }rem), 0, 0`;
+            }
+        };
 
-	useEffect(() => {
-		shiftCarousel(currentPage - 1);
-	}, [currentPage]);
+        shiftCarousel(currentPage - 1);
+    }, [currentPage]);
 
-	return (
-		<div className='carousel-container'>
-			<div className='container'>
-				<div className='carousel'>
-					{people.map((person) => (
-						<div key={person.id} className='carousel-item'>
-							<Card person={person} />
-						</div>
-					))}
-				</div>
-				<div className='carousel-nav'>
-					<button onClick={goLeft}>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							class='h-6 w-6'
-							fill='none'
-							viewBox='0 0 24 24'
-							stroke='currentColor'>
-							<path
-								stroke-linecap='round'
-								stroke-linejoin='round'
-								stroke-width='2'
-								d='M10 19l-7-7m0 0l7-7m-7 7h18'
-							/>
-						</svg>
-					</button>
-					<button onClick={goRight}>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							class='h-6 w-6'
-							fill='none'
-							viewBox='0 0 24 24'
-							stroke='currentColor'>
-							<path
-								stroke-linecap='round'
-								stroke-linejoin='round'
-								stroke-width='2'
-								d='M14 5l7 7m0 0l-7 7m7-7H3'
-							/>
-						</svg>
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className='carousel-container'>
+            <div className='container'>
+                <div className='carousel'>
+                    {people.map((person) => (
+                        <div key={person.id} className='carousel-item'>
+                            <Card person={person} />
+                        </div>
+                    ))}
+                </div>
+                <div className='carousel-nav'>
+                    <button onClick={goLeft}>
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            className='h-6 w-6'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'>
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                strokeWidth='2'
+                                d='M10 19l-7-7m0 0l7-7m-7 7h18'
+                            />
+                        </svg>
+                    </button>
+                    <button onClick={goRight}>
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            className='h-6 w-6'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'>
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                strokeWidth='2'
+                                d='M14 5l7 7m0 0l-7 7m7-7H3'
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Carousel;
